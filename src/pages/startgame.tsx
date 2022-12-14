cb63e91d518be5a0c557e7b7996f49855a47351e
Florian de Ghellinck <f.deghellinck@gmail.com>
2022-12-14 11:09:34 +0100

    fix:height

--- src/pages/startgame.tsx
+++ src/pages/startgame.tsx
@@ -6,7 +6,7 @@ const Form = () => {
     <div className='h-screen w-screen bg-gray-100 flex flex-col items-center font-poppins'>
       <Title title='QUIZ' />
       <h1 className='text-center text-3xl font-bold'> Règles du jeu</h1>
-      <div className='text-justify w-10/12 h-4/6 bg-embie-blue-dark-500 text-white p-4 rounded-lg mt-7'>
+      <div className='text-justify w-10/12 h-fit bg-embie-blue-dark-500 text-white p-4 rounded-lg mt-7'>
         <Rules />
       </div>
     </div>
